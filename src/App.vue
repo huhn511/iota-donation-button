ffaec5c1c56f1622691a370f794841b3edd0b858
huhn511 <sehe89@gmail.com>
2020-06-01 00:14:12 +0200

    :tada: add demo button

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <IotaDonationButton />
+</template>
+
+<script>
+import IotaDonationButton from './components/IotaDonationButton.vue'
+
+export default {
+  name: 'App',
+  components: {
+    IotaDonationButton
+  }
+}
+</script>
---
+++ src/components/IotaDonationButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="iota-donation-button" @click="count++">D {{ count }}</button>
+</template>
+
+<script>
+export default {
+  name: 'IotaDonationButton',
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+.iota-donation-button {
+  position: absolute;
+  top: 90%;
+  left: 80%;
+  display:block;
+  height: 60px;
+  width: 60px;
+  background-color: #abcdef;
+  color: white;
+  font-size: 20px;
+  border-radius: 50%;
+  border: 1px solid #fedcba;
+}
+
+</style>
